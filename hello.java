289152b421c8756eaca9a71f937e031b8ce84957
PankajInCode <pankaj.bhardwaj9315@gmail.com>
2025-01-24 09:02:16 +0530

    Check Positive or Negative Number

--- hello.java
+++ hello.java
@@ -1,5 +1,21 @@
+package Java;
+import java.util.Scanner;  // Import the Scanner class
+
+// 1. Check Positive or Negative Number
+
 public class hello{
     public static void main(String[] args){
-        System.out.print("Hello World")
-    } 
+        Scanner myObj = new Scanner(System.in);
+        try{
+            System.out.println("Enter Number...");
+            int number = myObj.nextInt();
+            if(number >= 0){
+                System.out.println("This Number is Positive...");
+            }else{
+                System.out.println("This Number is Negative...");
+            }
+        }finally{
+            myObj.close();
+        }
+    }
 }
